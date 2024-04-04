b0fe2f55e58be2c4a6e350c31740cf12fe64ba22
Vegar Engen <vegar.engen@gmail.com>
2024-04-04 23:00:08 +0200

    Lagt til Auth

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import { BrowserRouter} from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import Header from "./components/Header";
+import Progress from "./components/Progress";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'co'
-})
+  productionPrefix: "co",
+});
 
 export default () => {
-    return (
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>    
-                <div>
-                    <Header />
-                    <MarketingApp />
-                </div>
-            </BrowserRouter>
-        </StylesProvider>
-    )
-}
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
+  );
+};
